7d897c81fa8ff85249cf992b4dd3024caa6000cd
Enick Jhony <enick.jhony@gmail.com>
2023-05-02 19:30:34 -0400

    incluindo comentarios com useState do React

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,44 +1,65 @@
-import { format, formatDistanceToNow } from "date-fns";
-import ptBR from "date-fns/locale/pt-BR";
-import styles from './Post.module.css';
-import { Comment } from './Comment';
-import { Avatar } from './Avatar';
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import styles from './Post.module.css'
+import { Comment } from './Comment'
+import { Avatar } from './Avatar'
+import { useState } from 'react'
 
 export function Post({ author, publishedAt, content }) {
-  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH'h'mm", {
-    locale: ptBR,
-  })
+  const [comments, setComment] = useState([])
+  
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH'h'mm",
+    {
+      locale: ptBR
+    }
+  )
 
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     locale: ptBR,
-    addSuffix: true,
-  }) 
+    addSuffix: true
+  })
+
+  function handleCreateNewComment() {
+    event.preventDefault()
+
+    setComment([...comments, comments.length + 1])
+  }
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src={ author.avatarURL } />
+          <Avatar src={author.avatarURL} />
           <div className={styles.authorInfo}>
             <strong>{author.name}</strong>
             <span>{author.role}</span>
           </div>
         </div>
 
-        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
           {publishedDateRelativeToNow}
         </time>
       </header>
       <div className={styles.content}>
-        { content.map(line => {
-          if (line.type === "paragraph"){
-            return <p>{ line.content }</p>
-          } else if (line.type === "link"){
-            return <p><a href='#'>{line.content}</a></p>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p>{line.content}</p>
+          } else if (line.type === 'link') {
+            return (
+              <p>
+                <a href="#">{line.content}</a>
+              </p>
+            )
           }
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback!</strong>
 
         <textarea placeholder="Deixe um comentário!" />
@@ -49,8 +70,9 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment />
+        })}
       </div>
     </article>
   )
