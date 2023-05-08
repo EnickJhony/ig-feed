c239e787c85d47525d6dc0a3727c5b611846b2e2
Enick Jhony <enick.jhony@gmail.com>
2023-05-08 13:27:39 -0400

    Funcao like dentro do comentário

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,15 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
+import { useState } from 'react';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0)
+
+  function handleLike(){
+    setLikeCount(likeCount + 1)
+  }
+
   function handleDeleteComment(){
     onDeleteComment(content);
   }
@@ -34,9 +41,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLike}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
